68aa6dc9f001c8619920f26db1e748d795f655af
andreedgarian <andreedgarian@gmail.com>
2018-11-29 22:40:22 -0500

    boat shooting scene
    rough boat shooting scene

---
+++ ADVANCED Game/Assets/andreScene/shooting.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shooting : MonoBehaviour
+{
+
+    public GameObject Bullet_Emitter;
+
+
+    public GameObject Bullet;
+
+
+    public float Bullet_Forward_Force;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+          
+            GameObject Temporary_Bullet_Handler;
+            Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+
+          
+            //Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+
+           
+            Rigidbody Temporary_RigidBody;
+            Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+
+           
+            Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
+
+            
+            Destroy(Temporary_Bullet_Handler, 2.0f);
+        }
+    }
+}
---
+++ ADVANCED Game/Assets/andreScene/shootingEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootingEnemy : MonoBehaviour {
+
+    private Transform playerTransform;
+    private RaycastHit hit;
+    public float range = 400;
+
+    void Start()
+    {
+        playerTransform = transform.parent;
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //Debug.log("urDeadKiddo");
+
+            if (Physics.Raycast(playerTransform.TransformPoint (new Vector3(Random.Range(-1,1), Random.Range(-1,1),1)), playerTransform.forward, out hit, range))
+            {
+                if (hit.collider.tag == "target")
+                {
+                    Destroy(hit.transform.gameObject);
+                    Debug.Log("ShotEnemy");
+                }
+            }
+
+        }
+    }
+}
